1aaf11833425a8eea8254d268344fc19fe1c721d
Rafael B. Tauil <rafael@tauil.com.br>
2014-03-20 01:25:28 -0300

    Clear whitespaces

--- app/helpers/make_an_offer_helper.rb
+++ app/helpers/make_an_offer_helper.rb
@@ -3,12 +3,12 @@ module MakeAnOfferHelper
     case
     when !offer.rejected_at.nil?
       content_tag(:h3, t("rejected") + " " + offer.rejected_at.strftime("%m/%d/%Y"), :style => "color:#cc3300")
-      
+
     when !offer.accepted_at.nil?
       content_tag(:h3, t("accepted") + " " + offer.accepted_at.strftime("%m/%d/%Y"), :style => "color:#cc3300")
 
     else
-      t('notified_by_email_on_acceptance') 
-    end 
+      t('notified_by_email_on_acceptance')
+    end
   end
 end
