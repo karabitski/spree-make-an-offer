d64f630e59c62f38cc9e61be2cdd747731f8661f
Rafael B. Tauil <rafael@tauil.com.br>
2014-03-27 21:36:45 -0300

    Clear whitespaces and removes useless helper

--- app/controllers/users_controller_decorator.rb
+++ app/controllers/users_controller_decorator.rb
@@ -1,10 +1,6 @@
 Spree::UsersController.class_eval do
-  
-  #helper :products
-  
   def show_before
     @orders = @user.orders.checkout_complete
-    @offers = Offer.user_offers(@user.id).pending_offers.find(:all, :include => :product)      
+    @offers = Offer.user_offers(@user.id).pending_offers.find(:all, :include => :product)
   end
-
 end
