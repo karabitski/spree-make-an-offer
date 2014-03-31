67bb4bb99cff081aa89420979ea1f9f7a96da06d
Rafael B. Tauil <rafael@tauil.com.br>
2014-03-30 22:46:29 -0300

    Improves mail subjects

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -3,20 +3,20 @@ class OfferMailer < ActionMailer::Base
 
   def pending(offer)
     @offer = offer
-    subject = "#{Spree::Config[:site_name]} #{t('offer.email.pending.subject')} - #{offer.product.name}"
+    subject = "#{Spree::Config[:site_name]} - #{t('offer.email.pending.subject')} - #{offer.product.name}"
     mail(:to => Spree::MailMethod.first.preferred_smtp_username, :subject => subject)
   end
 
   def rejected(offer)
     @offer = offer
-    subject = "#{Spree::Config[:site_name]} #{t('offer.email.rejected.subject')} - #{offer.product.name}"
+    subject = "#{Spree::Config[:site_name]} - #{t('offer.email.rejected.subject')} - #{offer.product.name}"
     mail(:to => @offer.user.email, :subject => subject)
   end
 
   def accepted(offer, order)
     @offer = offer
     @order = order
-    subject = "#{Spree::Config[:site_name]} #{t('offer.email.accepted.subject')} - #{offer.product.name}"
+    subject = "#{Spree::Config[:site_name]} - #{t('offer.email.accepted.subject')} - #{offer.product.name}"
     mail(:to => @offer.user.email, :subject => subject)
   end
 end
