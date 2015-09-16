4c50f699107f1a2dda7ac5b9292760c77f739d5e
Andrei Karabitski <a.karabitski@gmail.com>
2015-09-16 13:35:17 +0300

    fixes for user account page

--- app/controllers/users_controller_decorator.rb
+++ app/controllers/users_controller_decorator.rb
@@ -3,6 +3,6 @@ Spree::UsersController.class_eval do
   before_filter :before_show, :only => [:show]
 
   def before_show
-    @offers = Spree::Offer.user_offers(@user.id).pending_offers.find(:all, :include => :product)
+    @offers = Spree::Offer.user_offers(@user.id).pending_offers.includes(:product)
   end
 end
