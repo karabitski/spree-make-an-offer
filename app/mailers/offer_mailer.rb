715362e6f30d921550b0ff9560584a3dc0bae332
Andrei Karabitski <a.karabitski@gmail.com>
2015-10-14 12:12:06 +0300

    inherit offer_mailer from spree base_mailer Include from_address to mail

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,35 +1,34 @@
-class OfferMailer < ActionMailer::Base
+class OfferMailer < Spree::BaseMailer
   helper "spree/base"
 
   def pending(offer)
     @offer = offer
     subject = "#{Spree::Config[:site_name]} - #{t('offer.email.pending.subject')} - #{offer.product.name}"
-    mail(:to => offer.store.user.email, :subject => subject)
+    mail(to: offer.store.user.email, from: from_address, subject: subject)
   end
 
   def rejected(offer)
     @offer = offer
     subject = "#{Spree::Config[:site_name]} - #{t('offer.email.rejected.subject')} - #{offer.product.name}"
-    mail(:to => @offer.user.email, :subject => subject)
+    mail(to: @offer.user.email, from: from_address, subject: subject)
   end
 
   def counter_offer(offer)
     @offer = offer
     subject = "#{Spree::Config[:site_name]} - #{t('offer.email.counter_offer.subject')} - #{offer.product.name}"
-    debugger
-    mail(:to => @offer.user.email, :subject => subject)
+    mail(to: @offer.user.email, from: from_address, subject: subject)
   end
 
   def counter_offer_accepted(offer)
     @offer = offer
     subject = "#{Spree::Config[:site_name]} - #{t('offer.email.counter_offer_accepted.subject')} - #{offer.product.name}"
-    mail(:to => offer.store.user.email, :subject => subject)
+    mail(to: offer.store.user.email, from: from_address, subject: subject)
   end
 
   def accepted(offer, order)
     @offer = offer
     @order = order
     subject = "#{Spree::Config[:site_name]} - #{t('offer.email.accepted.subject')} - #{offer.product.name}"
-    mail(:to => @offer.user.email, :subject => subject)
+    mail(to: @offer.user.email, from: from_address, subject: subject)
   end
 end
