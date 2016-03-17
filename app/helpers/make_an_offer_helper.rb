495ceb6d51d11feeed96c5a6a155c169d0bdefb2
Andrei Karabitski <a.karabitski@gmail.com>
2016-03-17 13:40:26 +0300

    fixed time formatting

--- app/helpers/make_an_offer_helper.rb
+++ app/helpers/make_an_offer_helper.rb
@@ -3,15 +3,15 @@ module MakeAnOfferHelper
   def offer_status(offer)
     case
     when offer.rejected_at.present?
-      content_tag(:span, t('offer_rejected', date: offer.rejected_at.strftime('%d/%m/%y %H:%m')), class: 'state canceled')
+      content_tag(:span, t('offer_rejected', date: offer.rejected_at.strftime('%d/%m/%y %H:%M')), class: 'state canceled')
     when offer.accepted_at.present?
-      content_tag(:span, t('offer_accepted', date: offer.accepted_at.strftime('%d/%m/%y %H:%m')), class: 'state complete')
+      content_tag(:span, t('offer_accepted', date: offer.accepted_at.strftime('%d/%m/%y %H:%M')), class: 'state complete')
     when offer.counter_price.present? && offer.counter_accepted.nil?
-      content_tag(:span, t('counter_offered', date: offer.updated_at.strftime('%d/%m/%y %H:%m')), class: 'state pending')
+      content_tag(:span, t('counter_offered', date: offer.updated_at.strftime('%d/%m/%y %H:%M')), class: 'state pending')
     when offer.counter_price.present? && offer.counter_accepted.present?
-      content_tag(:span, t('counter_offer_accepted', date: offer.counter_accepted.strftime('%d/%m/%y %H:%m')), class: 'state complete')
+      content_tag(:span, t('counter_offer_accepted', date: offer.counter_accepted.strftime('%d/%m/%y %H:%M')), class: 'state complete')
     else
-      content_tag(:span, t('offer_created', date: offer.created_at.strftime('%d/%m/%y %H:%m')), class: 'state pending')
+      content_tag(:span, t('offer_created', date: offer.created_at.strftime('%d/%m/%y %H:%M')), class: 'state pending')
     end
   end
 
